aec27434d9aa427b964a2a1fa380d2ca9289c616
Carlos Monteiro <offbyte@gmail.com>
2018-04-16 00:17:19 +0100

    exercise 07

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -18,12 +18,17 @@ package com.example.android.android_me.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.Toast;
 
 import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+import java.util.List;
 
 // This activity is responsible for displaying the master list of all images
 // Implement the MasterListFragment callback, OnImageClickListener
@@ -37,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
 
     // TODO (3) Create a variable to track whether to display a two-pane or single-pane UI
         // A single-pane display refers to phone screens, and two-pane to larger tablet screens
+    private boolean mTwoPane;
 
 
     @Override
@@ -46,7 +52,39 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
 
         // TODO (4) If you are making a two-pane display, add new BodyPartFragments to create an initial Android-Me image
         // Also, for the two-pane display, get rid of the "Next" button in the master list fragment
+        if (findViewById(R.id.android_me_linear_layout) != null) {
+            mTwoPane = true;
+            findViewById(R.id.next_button).setVisibility(View.GONE);
 
+            GridView gridView = (GridView) findViewById(R.id.images_grid_view);
+            gridView.setNumColumns(2);
+
+            if (savedInstanceState == null) {
+
+                addBodyFragment(R.id.head_container, AndroidImageAssets.getHeads());
+                addBodyFragment(R.id.body_container, AndroidImageAssets.getBodies());
+                addBodyFragment(R.id.leg_container, AndroidImageAssets.getLegs());
+            }
+        }
+
+
+    }
+
+    private void addBodyFragment(int container, List<Integer> imageIds) {
+        BodyPartFragment bodyPartFragment = new BodyPartFragment();
+        bodyPartFragment.setImageIds(imageIds);
+        getSupportFragmentManager().beginTransaction()
+                .add(container, bodyPartFragment)
+                .commit();
+    }
+
+    private void replaceBodyFragment(int container, List<Integer> imageIds, int listIndex) {
+        BodyPartFragment bodyPartFragment = new BodyPartFragment();
+        bodyPartFragment.setImageIds(imageIds);
+        bodyPartFragment.setListIndex(listIndex);
+        getSupportFragmentManager().beginTransaction()
+                .add(container, bodyPartFragment)
+                .commit();
     }
 
     // Define the behavior for onImageSelected
@@ -69,36 +107,52 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
         // This ensures that the index will always be a value between 0-11
         int listIndex = position - 12*bodyPartNumber;
 
-        // Set the currently displayed item for the correct body part fragment
-        switch(bodyPartNumber) {
-            case 0: headIndex = listIndex;
-                break;
-            case 1: bodyIndex = listIndex;
-                break;
-            case 2: legIndex = listIndex;
-                break;
-            default: break;
-        }
-
-        // Put this information in a Bundle and attach it to an Intent that will launch an AndroidMeActivity
-        Bundle b = new Bundle();
-        b.putInt("headIndex", headIndex);
-        b.putInt("bodyIndex", bodyIndex);
-        b.putInt("legIndex", legIndex);
-
-        // Attach the Bundle to an intent
-        final Intent intent = new Intent(this, AndroidMeActivity.class);
-        intent.putExtras(b);
-
-        // The "Next" button launches a new AndroidMeActivity
-        Button nextButton = (Button) findViewById(R.id.next_button);
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(intent);
+        if (mTwoPane) {
+            switch (bodyPartNumber) {
+                case 0:
+                    replaceBodyFragment(R.id.head_container,AndroidImageAssets.getHeads(), listIndex);
+                    break;
+                case 1:
+                    replaceBodyFragment(R.id.body_container,AndroidImageAssets.getBodies(), listIndex);
+                    break;
+                case 2:
+                    replaceBodyFragment(R.id.leg_container,AndroidImageAssets.getLegs(), listIndex);
+                    break;
+                default:
+                    break;
             }
-        });
 
+        } else {
+            // Set the currently displayed item for the correct body part fragment
+            switch(bodyPartNumber) {
+                case 0: headIndex = listIndex;
+                    break;
+                case 1: bodyIndex = listIndex;
+                    break;
+                case 2: legIndex = listIndex;
+                    break;
+                default: break;
+            }
+
+            // Put this information in a Bundle and attach it to an Intent that will launch an AndroidMeActivity
+            Bundle b = new Bundle();
+            b.putInt("headIndex", headIndex);
+            b.putInt("bodyIndex", bodyIndex);
+            b.putInt("legIndex", legIndex);
+
+            // Attach the Bundle to an intent
+            final Intent intent = new Intent(this, AndroidMeActivity.class);
+            intent.putExtras(b);
+
+            // The "Next" button launches a new AndroidMeActivity
+            Button nextButton = (Button) findViewById(R.id.next_button);
+            nextButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(intent);
+                }
+            });
+        }
     }
 
 }
